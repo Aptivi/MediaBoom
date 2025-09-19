3723bc92f3fbaba701bc2af8aae1e0f631410c0e
AptiviCEO <AptiviCEO@users.noreply.github.com>
2025-09-19 12:53:54 +0000

    int - Updated libmpv links

--- vnd/vendor-prebuild.cmd
+++ vnd/vendor-prebuild.cmd
@@ -3,17 +3,17 @@
 set ROOTDIR=%~dp0\..
 
 REM Download libmpv for Windows and build
-if not exist "%TEMP%\mpv-dev-x86_64-20250918-git-764da99.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-09-18-764da99/mpv-dev-x86_64-20250918-git-764da99.7z -OutFile ""%TEMP%\mpv-dev-x86_64-20250918-git-764da99.7z"""
-if not exist "%TEMP%\mpv-dev-aarch64-20250918-git-764da99.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-09-18-764da99/mpv-dev-aarch64-20250918-git-764da99.7z -OutFile ""%TEMP%\mpv-dev-aarch64-20250918-git-764da99.7z"""
+if not exist "%TEMP%\mpv-dev-x86_64-20250919-git-0bbcc91.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-09-19-0bbcc91/mpv-dev-x86_64-20250919-git-0bbcc91.7z -OutFile ""%TEMP%\mpv-dev-x86_64-20250919-git-0bbcc91.7z"""
+if not exist "%TEMP%\mpv-dev-aarch64-20250919-git-0bbcc91.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-09-19-0bbcc91/mpv-dev-aarch64-20250919-git-0bbcc91.7z -OutFile ""%TEMP%\mpv-dev-aarch64-20250919-git-0bbcc91.7z"""
 
 pushd "%ROOTDIR%\tools\"
-"%ProgramFiles%\7-Zip\7z.exe" x "%TEMP%\mpv-dev-x86_64-20250918-git-764da99.7z" libmpv-2.dll
+"%ProgramFiles%\7-Zip\7z.exe" x "%TEMP%\mpv-dev-x86_64-20250919-git-0bbcc91.7z" libmpv-2.dll
 popd
 mkdir "%ROOTDIR%\public\BassBoom.Native\runtimes\win-x64\native\"
 move "%ROOTDIR%\tools\libmpv-2.dll" "%ROOTDIR%\public\BassBoom.Native\runtimes\win-x64\native\"
 
 pushd "%ROOTDIR%\tools\"
-"%ProgramFiles%\7-Zip\7z.exe" x "%TEMP%\mpv-dev-aarch64-20250918-git-764da99.7z" libmpv-2.dll
+"%ProgramFiles%\7-Zip\7z.exe" x "%TEMP%\mpv-dev-aarch64-20250919-git-0bbcc91.7z" libmpv-2.dll
 popd
 mkdir "%ROOTDIR%\public\BassBoom.Native\runtimes\win-arm64\native\"
 move "%ROOTDIR%\tools\libmpv-2.dll" "%ROOTDIR%\public\BassBoom.Native\runtimes\win-arm64\native\"
