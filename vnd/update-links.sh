5a40023631870886fa4cf692e6efd09bb57c106f
AptiviCEO <AptiviCEO@users.noreply.github.com>
2025-09-24 12:54:47 +0000

    int - Updated libmpv links

--- vnd/update-links.sh
+++ vnd/update-links.sh
@@ -19,9 +19,9 @@ sedpath=$(which sed)
 checkerror $? "sed is not found"
 
 # Below variables to replace when script is complete
-OLDREV="2025-09-23-ee0f701"
-OLDFILENAMESTD="mpv-dev-x86_64-20250923-git-ee0f701.7z"
-OLDFILENAMEARM="mpv-dev-aarch64-20250923-git-ee0f701.7z"
+OLDREV="2025-09-24-be98b35"
+OLDFILENAMESTD="mpv-dev-x86_64-20250924-git-be98b35.7z"
+OLDFILENAMEARM="mpv-dev-aarch64-20250924-git-be98b35.7z"
 
 # Get the new links
 echo "Checking for updates..."
--- vnd/vendor-prebuild.cmd
+++ vnd/vendor-prebuild.cmd
@@ -3,17 +3,17 @@
 set ROOTDIR=%~dp0\..
 
 REM Download libmpv for Windows and build
-if not exist "%TEMP%\mpv-dev-x86_64-20250923-git-ee0f701.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-09-23-ee0f701/mpv-dev-x86_64-20250923-git-ee0f701.7z -OutFile ""%TEMP%\mpv-dev-x86_64-20250923-git-ee0f701.7z"""
-if not exist "%TEMP%\mpv-dev-aarch64-20250923-git-ee0f701.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-09-23-ee0f701/mpv-dev-aarch64-20250923-git-ee0f701.7z -OutFile ""%TEMP%\mpv-dev-aarch64-20250923-git-ee0f701.7z"""
+if not exist "%TEMP%\mpv-dev-x86_64-20250924-git-be98b35.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-09-24-be98b35/mpv-dev-x86_64-20250924-git-be98b35.7z -OutFile ""%TEMP%\mpv-dev-x86_64-20250924-git-be98b35.7z"""
+if not exist "%TEMP%\mpv-dev-aarch64-20250924-git-be98b35.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-09-24-be98b35/mpv-dev-aarch64-20250924-git-be98b35.7z -OutFile ""%TEMP%\mpv-dev-aarch64-20250924-git-be98b35.7z"""
 
 pushd "%ROOTDIR%\tools\"
-"%ProgramFiles%\7-Zip\7z.exe" x "%TEMP%\mpv-dev-x86_64-20250923-git-ee0f701.7z" libmpv-2.dll
+"%ProgramFiles%\7-Zip\7z.exe" x "%TEMP%\mpv-dev-x86_64-20250924-git-be98b35.7z" libmpv-2.dll
 popd
 mkdir "%ROOTDIR%\public\BassBoom.Native\runtimes\win-x64\native\"
 move "%ROOTDIR%\tools\libmpv-2.dll" "%ROOTDIR%\public\BassBoom.Native\runtimes\win-x64\native\"
 
 pushd "%ROOTDIR%\tools\"
-"%ProgramFiles%\7-Zip\7z.exe" x "%TEMP%\mpv-dev-aarch64-20250923-git-ee0f701.7z" libmpv-2.dll
+"%ProgramFiles%\7-Zip\7z.exe" x "%TEMP%\mpv-dev-aarch64-20250924-git-be98b35.7z" libmpv-2.dll
 popd
 mkdir "%ROOTDIR%\public\BassBoom.Native\runtimes\win-arm64\native\"
 move "%ROOTDIR%\tools\libmpv-2.dll" "%ROOTDIR%\public\BassBoom.Native\runtimes\win-arm64\native\"
