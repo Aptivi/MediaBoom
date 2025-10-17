edbe26abfaf9ab67dbf8523b0907a43d33b8c4f8
AptiviCEO <AptiviCEO@users.noreply.github.com>
2025-10-17 12:55:48 +0000

    int - Updated libmpv links

--- vnd/vendor-prelocalize.cmd
+++ vnd/vendor-prelocalize.cmd
@@ -3,5 +3,5 @@
 set ROOTDIR=%~dp0\..
 
 REM Download libmpv for Windows and build
-if not exist "%TEMP%\" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-10-16-233e896/ -OutFile ""%TEMP%\"""
-if not exist "%TEMP%\" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-10-16-233e896/ -OutFile ""%TEMP%\"""
+if not exist "%TEMP%\" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-10-17-233e896/ -OutFile ""%TEMP%\"""
+if not exist "%TEMP%\" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-10-17-233e896/ -OutFile ""%TEMP%\"""
