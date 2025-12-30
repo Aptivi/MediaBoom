2ddb85d41ee70de7d4c2c08f44371b0c0cbba68c
AptiviCEO <AptiviCEO@users.noreply.github.com>
2025-12-30 13:02:46 +0000

    int - Updated libmpv links

--- vnd/update-links.sh
+++ vnd/update-links.sh
@@ -19,9 +19,9 @@ sedpath=$(which sed)
 checkerror $? "sed is not found"
 
 # Below variables to replace when script is complete
-OLDREV="2025-12-29-468e2a8"
-OLDFILENAMESTD="mpv-dev-x86_64-20251229-git-468e2a8.7z"
-OLDFILENAMEARM="mpv-dev-aarch64-20251229-git-468e2a8.7z"
+OLDREV="2025-12-30-468e2a8"
+OLDFILENAMESTD="mpv-dev-x86_64-20251230-git-468e2a8.7z"
+OLDFILENAMEARM="mpv-dev-aarch64-20251230-git-468e2a8.7z"
 
 # Get the new links
 echo "Checking for updates..."
--- vnd/vendor-prelocalize.cmd
+++ vnd/vendor-prelocalize.cmd
@@ -3,5 +3,5 @@
 set ROOTDIR=%~dp0\..
 
 REM Download libmpv for Windows and build
-if not exist "%TEMP%\mpv-dev-x86_64-20251229-git-468e2a8.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-12-29-468e2a8/mpv-dev-x86_64-20251229-git-468e2a8.7z -OutFile ""%TEMP%\mpv-dev-x86_64-20251229-git-468e2a8.7z"""
-if not exist "%TEMP%\mpv-dev-aarch64-20251229-git-468e2a8.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-12-29-468e2a8/mpv-dev-aarch64-20251229-git-468e2a8.7z -OutFile ""%TEMP%\mpv-dev-aarch64-20251229-git-468e2a8.7z"""
+if not exist "%TEMP%\mpv-dev-x86_64-20251230-git-468e2a8.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-12-30-468e2a8/mpv-dev-x86_64-20251230-git-468e2a8.7z -OutFile ""%TEMP%\mpv-dev-x86_64-20251230-git-468e2a8.7z"""
+if not exist "%TEMP%\mpv-dev-aarch64-20251230-git-468e2a8.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-12-30-468e2a8/mpv-dev-aarch64-20251230-git-468e2a8.7z -OutFile ""%TEMP%\mpv-dev-aarch64-20251230-git-468e2a8.7z"""
