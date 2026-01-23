211933ecd4663ecfbfb52cd577236052a3e14071
AptiviCEO <AptiviCEO@users.noreply.github.com>
2026-01-23 13:07:05 +0000

    int - Updated libmpv links

--- vnd/vendor-prebuild.cmd
+++ vnd/vendor-prebuild.cmd
@@ -3,17 +3,17 @@
 set ROOTDIR=%~dp0\..
 
 REM Download libmpv for Windows and build
-if not exist "%TEMP%\mpv-dev-x86_64-20260122-git-df51d55.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2026-01-22-df51d55/mpv-dev-x86_64-20260122-git-df51d55.7z -OutFile ""%TEMP%\mpv-dev-x86_64-20260122-git-df51d55.7z"""
-if not exist "%TEMP%\mpv-dev-aarch64-20260122-git-df51d55.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2026-01-22-df51d55/mpv-dev-aarch64-20260122-git-df51d55.7z -OutFile ""%TEMP%\mpv-dev-aarch64-20260122-git-df51d55.7z"""
+if not exist "%TEMP%\mpv-dev-x86_64-20260123-git-dd50a47.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2026-01-23-dd50a47/mpv-dev-x86_64-20260123-git-dd50a47.7z -OutFile ""%TEMP%\mpv-dev-x86_64-20260123-git-dd50a47.7z"""
+if not exist "%TEMP%\mpv-dev-aarch64-20260123-git-dd50a47.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2026-01-23-dd50a47/mpv-dev-aarch64-20260123-git-dd50a47.7z -OutFile ""%TEMP%\mpv-dev-aarch64-20260123-git-dd50a47.7z"""
 
 pushd "%ROOTDIR%\tools\"
-"%ProgramFiles%\7-Zip\7z.exe" x "%TEMP%\mpv-dev-x86_64-20260122-git-df51d55.7z" libmpv-2.dll
+"%ProgramFiles%\7-Zip\7z.exe" x "%TEMP%\mpv-dev-x86_64-20260123-git-dd50a47.7z" libmpv-2.dll
 popd
 mkdir "%ROOTDIR%\public\MediaBoom.Native\runtimes\win-x64\native\"
 move "%ROOTDIR%\tools\libmpv-2.dll" "%ROOTDIR%\public\MediaBoom.Native\runtimes\win-x64\native\"
 
 pushd "%ROOTDIR%\tools\"
-"%ProgramFiles%\7-Zip\7z.exe" x "%TEMP%\mpv-dev-aarch64-20260122-git-df51d55.7z" libmpv-2.dll
+"%ProgramFiles%\7-Zip\7z.exe" x "%TEMP%\mpv-dev-aarch64-20260123-git-dd50a47.7z" libmpv-2.dll
 popd
 mkdir "%ROOTDIR%\public\MediaBoom.Native\runtimes\win-arm64\native\"
 move "%ROOTDIR%\tools\libmpv-2.dll" "%ROOTDIR%\public\MediaBoom.Native\runtimes\win-arm64\native\"
--- vnd/vendor-prelocalize.cmd
+++ vnd/vendor-prelocalize.cmd
@@ -3,5 +3,5 @@
 set ROOTDIR=%~dp0\..
 
 REM Download libmpv for Windows and build
-if not exist "%TEMP%\mpv-dev-x86_64-20260122-git-df51d55.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2026-01-22-df51d55/mpv-dev-x86_64-20260122-git-df51d55.7z -OutFile ""%TEMP%\mpv-dev-x86_64-20260122-git-df51d55.7z"""
-if not exist "%TEMP%\mpv-dev-aarch64-20260122-git-df51d55.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2026-01-22-df51d55/mpv-dev-aarch64-20260122-git-df51d55.7z -OutFile ""%TEMP%\mpv-dev-aarch64-20260122-git-df51d55.7z"""
+if not exist "%TEMP%\mpv-dev-x86_64-20260123-git-dd50a47.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2026-01-23-dd50a47/mpv-dev-x86_64-20260123-git-dd50a47.7z -OutFile ""%TEMP%\mpv-dev-x86_64-20260123-git-dd50a47.7z"""
+if not exist "%TEMP%\mpv-dev-aarch64-20260123-git-dd50a47.7z" powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2026-01-23-dd50a47/mpv-dev-aarch64-20260123-git-dd50a47.7z -OutFile ""%TEMP%\mpv-dev-aarch64-20260123-git-dd50a47.7z"""
