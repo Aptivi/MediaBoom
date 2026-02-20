4a5b3505089a78d041ec2d9a48354472de81972e
AptiviCEO <AptiviCEO@users.noreply.github.com>
2026-02-20 13:20:33 +0000

    int - Updated libmpv links

--- vnd/update-links.sh
+++ vnd/update-links.sh
@@ -19,9 +19,9 @@ sedpath=$(which sed)
 checkerror $? "sed is not found"
 
 # Below variables to replace when script is complete
-OLDREV="2026-02-19-c2c081f"
-OLDFILENAMESTD="mpv-dev-x86_64-20260219-git-c2c081f.7z"
-OLDFILENAMEARM="mpv-dev-aarch64-20260219-git-c2c081f.7z"
+OLDREV="2026-02-20-f7ad4a5"
+OLDFILENAMESTD="mpv-dev-x86_64-20260220-git-f7ad4a5.7z"
+OLDFILENAMEARM="mpv-dev-aarch64-20260220-git-f7ad4a5.7z"
 
 # Get the new links
 echo "Checking for updates..."
--- vnd/vendor.sh
+++ vnd/vendor.sh
@@ -16,8 +16,8 @@ localize() {
 
     # Download libmpv for Windows
     echo "Downloading libmpv for Windows..."
-    curl -L --output "$ROOTDIR/vnd/" https://github.com/zhongfly/mpv-winbuild/releases/download/2026-02-19-c2c081f/
-    curl -L --output "$ROOTDIR/vnd/" https://github.com/zhongfly/mpv-winbuild/releases/download/2026-02-19-c2c081f/
+    curl -L --output "$ROOTDIR/vnd/" https://github.com/zhongfly/mpv-winbuild/releases/download/2026-02-20-f7ad4a5/
+    curl -L --output "$ROOTDIR/vnd/" https://github.com/zhongfly/mpv-winbuild/releases/download/2026-02-20-f7ad4a5/
 
     # Copy dependencies to the "deps" folder underneath the root directory
     mkdir -p "$ROOTDIR/deps"
@@ -54,11 +54,11 @@ prebuild() {
 
     # Download compiled Windows libmpv libraries
     if [ ! -f $ROOTDIR/vnd/ ]; then
-        curl -L --output $ROOTDIR/vnd/ https://github.com/zhongfly/mpv-winbuild/releases/download/2026-02-19-c2c081f/
+        curl -L --output $ROOTDIR/vnd/ https://github.com/zhongfly/mpv-winbuild/releases/download/2026-02-20-f7ad4a5/
         checkvendorerror $?
     fi
     if [ ! -f $ROOTDIR/vnd/ ]; then
-        curl -L --output $ROOTDIR/vnd/ https://github.com/zhongfly/mpv-winbuild/releases/download/2026-02-19-c2c081f/
+        curl -L --output $ROOTDIR/vnd/ https://github.com/zhongfly/mpv-winbuild/releases/download/2026-02-20-f7ad4a5/
         checkvendorerror $?
     fi
 
