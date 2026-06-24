81a7a9a2dedea414bbf2675c604517bd90142778
AptiviCEO <AptiviCEO@users.noreply.github.com>
2026-06-24 14:50:02 +0000

    int - Updated libmpv links

--- vendor/update-links.sh
+++ vendor/update-links.sh
@@ -19,9 +19,9 @@ sedpath=$(which sed)
 checkerror $? "sed is not found"
 
 # Below variables to replace when script is complete
-OLDREV="2026-06-23-2339eb7276"
-OLDFILENAMESTD="mpv-dev-x86_64-20260623-git-2339eb7276.7z"
-OLDFILENAMEARM="mpv-dev-aarch64-20260623-git-2339eb7276.7z"
+OLDREV="2026-06-24-2339eb7276"
+OLDFILENAMESTD="mpv-dev-x86_64-20260624-git-2339eb7276.7z"
+OLDFILENAMEARM="mpv-dev-aarch64-20260624-git-2339eb7276.7z"
 
 # Get the new links
 echo "Checking for updates..."
