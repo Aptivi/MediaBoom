2e4362f5c422605d34ca29a702005bf09b9cfc1f
AptiviCEO <AptiviCEO@users.noreply.github.com>
2026-07-04 13:53:42 +0000

    int - Updated libmpv links

--- vendor/libmpv_win.py
+++ vendor/libmpv_win.py
@@ -6,9 +6,9 @@ import os
 def download_libmpv_win(root_dir, extract: bool = True):
     # Download LibMPV for Windows
     output_dir = root_dir + "/libmpv-win"
-    revision = "2026-07-03-7aa8580cab"
-    filename_amd = "mpv-dev-x86_64-20260703-git-7aa8580cab.7z"
-    filename_arm = "mpv-dev-aarch64-20260703-git-7aa8580cab.7z"
+    revision = "2026-07-04-33111f3212"
+    filename_amd = "mpv-dev-x86_64-20260704-git-33111f3212.7z"
+    filename_arm = "mpv-dev-aarch64-20260704-git-33111f3212.7z"
     link_base = "https://github.com/zhongfly/mpv-winbuild/releases/download/"
     path_amd = output_dir + '/' + filename_amd
     path_arm = output_dir + '/' + filename_arm
--- vendor/update-links.sh
+++ vendor/update-links.sh
@@ -19,9 +19,9 @@ sedpath=$(which sed)
 checkerror $? "sed is not found"
 
 # Below variables to replace when script is complete
-OLDREV="2026-07-03-7aa8580cab"
-OLDFILENAMESTD="mpv-dev-x86_64-20260703-git-7aa8580cab.7z"
-OLDFILENAMEARM="mpv-dev-aarch64-20260703-git-7aa8580cab.7z"
+OLDREV="2026-07-04-33111f3212"
+OLDFILENAMESTD="mpv-dev-x86_64-20260704-git-33111f3212.7z"
+OLDFILENAMEARM="mpv-dev-aarch64-20260704-git-33111f3212.7z"
 
 # Get the new links
 echo "Checking for updates..."
