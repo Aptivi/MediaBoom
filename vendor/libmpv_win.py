b20d90753cb9c36b829efeb9543f7e0ee6686470
AptiviCEO <AptiviCEO@users.noreply.github.com>
2026-07-15 14:03:13 +0000

    int - Updated libmpv links

--- vendor/libmpv_win.py
+++ vendor/libmpv_win.py
@@ -6,9 +6,9 @@ import os
 def download_libmpv_win(root_dir, extract: bool = True):
     # Download LibMPV for Windows
     output_dir = root_dir + "/libmpv-win"
-    revision = "2026-07-14-e5486b96d7"
-    filename_amd = "mpv-dev-x86_64-20260714-git-e5486b96d7.7z"
-    filename_arm = "mpv-dev-aarch64-20260714-git-e5486b96d7.7z"
+    revision = "2026-07-15-94335ab87a"
+    filename_amd = "mpv-dev-x86_64-20260715-git-94335ab87a.7z"
+    filename_arm = "mpv-dev-aarch64-20260715-git-94335ab87a.7z"
     link_base = "https://github.com/zhongfly/mpv-winbuild/releases/download/"
     path_amd = output_dir + '/' + filename_amd
     path_arm = output_dir + '/' + filename_arm
